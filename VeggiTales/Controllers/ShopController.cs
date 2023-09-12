52027c7cd61181453ae71ca5aaa3af13ccc6e97d
ifotn <rich@infrontofthenet.com>
2023-09-12 08:54:33 -0400

    Add project files.

---
+++ VeggiTales/Controllers/ShopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VeggiTales.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

fb6437ef53782723cd0273dcc9bbb7f9b96a08ac
ifotn <rich@infrontofthenet.com>
2023-09-12 09:59:54 -0400

    l2/2 - shop by category

--- VeggiTales/Controllers/ShopController.cs
+++ VeggiTales/Controllers/ShopController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VeggiTales.Models;
 
 namespace VeggiTales.Controllers
 {
@@ -7,6 +8,30 @@ namespace VeggiTales.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        // GET: /Shop/ByCategory/{name}
+        public IActionResult ByCategory(string name)
+        {
+            if (name == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            // use ViewData dictionary to pass the category name to the view for display
+            ViewData["Category"] = name;
+
+            // use the Product model to make a mock list of products for display
+            // this is strongly-typed data so we won't use ViewData
+            var products = new List<Product>();
+
+            for (int i = 1; i < 11; i++)
+            {
+                products.Add(new Product { ProductId = i, Name = "Product " + i.ToString() });
+            }
+
+            // pass the data to the view for display
+            return View(products);
         }
     }
 }

8478f803472e97ca0d7530d844b8e24f2e7f5275
ifotn <rich@infrontofthenet.com>
2023-09-26 09:50:51 -0400

    l4/1 - create store models

--- VeggiTales/Models/Product.cs
+++ VeggiTales/Models/Product.cs
@@ -5,5 +5,13 @@
         public int ProductId { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+
+        // add reference to the parent Category
+        public Category Category { get; set; }
+
+        // add child refs
+        public List<CartItem> CartItems { get; set; }
+        public List<OrderDetail> OrderDetails { get; set; }
     }
 }

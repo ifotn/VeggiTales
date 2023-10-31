f456308da0d0b1bb7b3762efbb7858413d3fdf2b
ifotn <rich@infrontofthenet.com>
2023-10-31 10:01:21 -0400

    l8/2 - authorization enabled

--- VeggiTales/Controllers/CategoriesController.cs
+++ VeggiTales/Controllers/CategoriesController.cs
@@ -11,7 +11,7 @@ using VeggiTales.Models;
 
 namespace VeggiTales.Controllers
 {
-    [Authorize]
+    [Authorize(Roles = "Administrator")]
     public class CategoriesController : Controller
     {
         private readonly ApplicationDbContext _context;
--- VeggiTales/Controllers/ProductsController.cs
+++ VeggiTales/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using VeggiTales.Models;
 
 namespace VeggiTales.Controllers
 {
+    [Authorize(Roles = "Administrator")]
     public class ProductsController : Controller
     {
         private readonly ApplicationDbContext _context;
@@ -26,6 +28,7 @@ namespace VeggiTales.Controllers
             return View(await applicationDbContext.ToListAsync());
         }
 
+        [AllowAnonymous]
         // GET: Products/Details/5
         public async Task<IActionResult> Details(int? id)
         {

4bdac37aba56deae4672cd12cbe37f0610a42558
ifotn <Rich.Freeman@GeorgianCollege.ca>
2023-11-28 08:55:58 -0500

    l11/1 - unit test setup

--- VeggiTales/Controllers/HomeController.cs
+++ VeggiTales/Controllers/HomeController.cs
@@ -6,21 +6,21 @@ namespace VeggiTales.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        //private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
+            //_logger = logger;
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View("Index");
         }
 
         public IActionResult Privacy()
         {
-            return View();
+            return View("Privacy");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
